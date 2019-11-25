6cda11fb5eaab334320a3471f31b80ea314519a0
zZMathSP <zzmathsp@gmail.com>
2019-11-24 21:39:37 -0400

    trocando o layout do campo

--- src/arena/Arena.java
+++ src/arena/Arena.java
@@ -84,8 +84,7 @@ public class Arena {
     for(int i=0;i<dimensao;i++){
         for(int j=0;j<dimensao;j++){
             for(int z=0;z<dimensao;z++){
-                campo.add("*");
-
+                campo.add("█");// itens = ░   
             }             
         }
         campo.add ("\n");
